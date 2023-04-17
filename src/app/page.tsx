5f8685d19b2f26f703ce6a08b26ade893c3a0b3e
VInicius Tonashiro <vinit.tonashiro@hotmail.com>
2023-04-17 19:03:02 -0300

    fix: vercel error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
-import { Component } from "@/components/Shared/Component";
+import { Component } from "@shared/Component";
 
 const inter = Inter({ subsets: ["latin"] });
 
